fcb8fda0716a0ad393cad8854f26bc8c0c469fd2
Gennadiy Falatiuk <gena.fox.gf@gmail.com>
2018-08-25 00:32:06 +0300

    Playground: Functions & Headers

---
+++ Playground/Training/Functions.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+int sum(int x, int y) {
+    return x + y;
+}
+
+double sum(double x, double y) {
+    return x + y;
+}
---
+++ Playground/Training/Functions.h
@@ -0,0 +1,2 @@
+int sum(int x, int y);
+double sum(double x, double y); 
